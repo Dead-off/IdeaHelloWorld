663bd26fcf8fc5e9d0f2db285279a6cfa73f8eea
Zaytsev Maxim <Maxim.Zaytsev@jetbrains.com>
2020-01-17 11:57:44 +0300

    debug

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" [gant]   ###<<<>>>###: hello wd(remote run)");
+        System.out.println("###<<<>>>###: hello wd(remote run)");
     }
 }
