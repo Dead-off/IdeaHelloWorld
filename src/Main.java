f9df77d00bbf6b0bb8c323fb417a7bbdd5b54da7
Zaytsev Maxim <Maxim.Zaytsev@jetbrains.com>
2020-02-19 17:01:03 +0300

    ch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         //System.out.println("hello wd(remote run)");
-        System.out.println("###<<<>>>###: hello wd(remote run)");
+        System.out.println("good job hello wd(remote run)");
     }
 }
