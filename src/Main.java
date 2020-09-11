da6a94ffbace52719aa9e3d18a1893817e555d00
Zaytsev Maxim <Maxim.Zaytsev@jetbrains.com>
2020-09-11 14:14:06 +0300

    meow

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("hello wd(remote run)");
 	System.out.println(System.getProperty("java.io.tmpdir"));
-        System.out.println("good josswqqqqqqqwwsswwqqqasdasqqqqwqwqqwwwwwdwqwqqwwqqqqwwqqqwwqwqwqweewqwwewwweewwewwswqqqwqwssqb hello wd(remote run)");
+        System.out.println("good josswqqqqqqqwwsswwqqqasdasqqqqwqwqwqqwwwwwdwqwqqwwqqqqwwqqqwwqwqwqweewqwwewwweewwewwswqqqwqwssqb hello wd(remote run)");
     }
 }
