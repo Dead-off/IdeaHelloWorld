716ee1203b824f4c9d9bf6f2563fa9b5d55dcfc1
Zaytsev Maxim <Maxim.Zaytsev@jetbrains.com>
2020-09-30 16:51:52 +0300

    rel2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         //System.out.println("hello wd(remote run)");
-        System.out.println("branch hello wd(remote runrqwrww)q");
+        System.out.println("branch hello wd(remote runrqwrww)wq:q");
     }
 }
