0a1deb43933216839ec87e9a1e9f903898671ef4
Maxim Zaytsev <maxim.zaytsev@jetbrains.com>
2023-03-24 14:47:40 +0100

    change

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("hello wd(remote run)");
 	System.out.println(System.getProperty("java.io.tmpdir"));
-        System.out.println("good iiiiiiiiiiiiiiiqjosswerqwetwqqqqqqqwwsswwqqqasdasqqqq21wwqwwqqwweqiqwqwqqwwwwwdwqwqqwwqqqqwwqqqwwqwqwqweewqwwewwweewwewwswqqqwqwssqb hello wd(remote run)");
+        System.out.println("good iiiiiiiiiiiiiiiiqjosswerqwetwqqqqqqqwwsswwqqqasdasqqqq21wwqwwqqwweqiqwqwqqwwwwwdwqwqqwwqqqqwwqqqwwqwqwqweewqwwewwweewwewwswqqqwqwssqb hello wd(remote run)");
     }
 }
